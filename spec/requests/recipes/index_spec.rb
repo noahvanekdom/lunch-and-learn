91bb02d1f83f7a77ca58a95bb0bd8eb78004c439
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-13 16:44:40 -0700

    Add test framework

---
+++ spec/requests/recipes/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'the recipe index API response (api/vi/recipes?country={query})' do
+  context 'happy paths' do
+    it 'returns a json response with an array of recipe objects' do
+
+    end
+
+    it 'the response contains the correct attributes for each recipe' do 
+
+    end
+
+  end
+
+  context 'sad paths' do
+    describe 'when an incorrect key is used' do
+
+    end
+    describe 'when no recipes are returned' do
+
+    end
+  end
+end
