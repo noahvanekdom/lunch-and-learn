a201afe912e3b430d279d22e821fc079a85cb90c
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-13 16:54:07 -0700

    Finish test setup

--- spec/requests/recipes/index_spec.rb
+++ spec/requests/recipes/index_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'the recipe index API response (api/vi/recipes?country={query})' do
+RSpec.describe 'the recipe index API response (api/vi/recipes?country={query})', type: :request  do
   context 'happy paths' do
     it 'returns a json response with an array of recipe objects' do
 
+      country = "spain"
+
+      get "/api/v1/recipes?country=#{country}"
+
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data]).to be_a Array
     end
 
     it 'the response contains the correct attributes for each recipe' do 
