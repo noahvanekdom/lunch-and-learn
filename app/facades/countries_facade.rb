30b16139d5810386b966f4cbcd7bb9d67e4dfc71
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-14 11:56:12 -0700

    Clean up country facade tests

--- app/facades/countries_facade.rb
+++ app/facades/countries_facade.rb
@@ -4,12 +4,11 @@ class CountriesFacade
     response.pop[:name]
   end
 
-  def self.country_details(country)
-    response = CountriesService.details(country)
-  end
+  # def self.country_details(country)
+  #   response = CountriesService.details(country)
+  # end
 
   def self.lat_long(country)
-    require 'pry'; binding.pry
     response = CountriesService.details(country).first[:latlng]
   end
 end
