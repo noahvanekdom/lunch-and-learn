4a61950eece24515b3a19e4b84e4ef09ab7401ae
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-14 10:23:10 -0700

    Fix typos in service to allow it to reach the same endpoint as postman example

--- app/services/places_service.rb
+++ app/services/places_service.rb
@@ -1,15 +1,15 @@
 class PlacesService
 
   def self.conn
-    Faraday.new("https://api.geoapify.com/v2") do |faraday|
+    Faraday.new("https://api.geoapify.com") do |faraday|
       faraday.params['apiKey'] = ENV['geoapify_api_key']
     end
   end
 
   def self.tourist_sights(lat_long)
-    conn.get("/places") do |faraday|
+    response = conn.get("/v2/places") do |faraday|
       faraday.params['categories'] = 'tourism.sights'
-      faraday.params['filter'] = ["circle:#{lat_long[1]},#{lat_long[0]},20000"]
+      faraday.params['filter'] = "circle:#{lat_long[1]},#{lat_long[0]},20000"
     end
   end
 end
