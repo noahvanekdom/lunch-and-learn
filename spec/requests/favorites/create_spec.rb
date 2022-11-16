6c48a6c9b4941b5d4aebd0d7db252a477d289a03
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-15 20:18:12 -0700

    Flesh out favorite create spec

--- spec/requests/favorites/create_spec.rb
+++ spec/requests/favorites/create_spec.rb
@@ -17,7 +17,11 @@ RSpec.describe 'the create favorite API endpoint' do
       end
 
       it "creates a new favorite" do
+        expect(@user.favorites).to eq([])
+
         post('/api/v1/favorites', headers: @headers, params: JSON.generate(@body))
+
+        expect(@user.favorites.count).to eq 1
       end
 
       it "returns a message indicating successful creation and a 201 status code" do
@@ -27,9 +31,33 @@ RSpec.describe 'the create favorite API endpoint' do
 
 
         expect(response).to be_successful
-        expect(response.body).to eq({ "success": "Favorite added successfully" })
+        expect(message).to eq({:success=>"Favorite added successfully"})
+
+        favorite = Favorite.last
+        expect(favorite.user).to eq(@user)
+        expect(favorite.country).to eq("thailand")
+        expect(favorite.recipe_link).to eq("https://www.tastingtable.com/")
+        expect(favorite.recipe_title).to eq("Crab Fried Rice (Khaao Pad Bpu)")
+      end
+    end
+
+    describe 'sad path' do
+      context 'when an invalid api key is provided' do
+        it 'returns a 404 status with message' do
+          body = {
+            "api_key": "FAILKEY",
+            "country": "thailand",
+            "recipe_link": "https://www.tastingtable.com/",
+            "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)"
+        }
+          post('/api/v1/favorites', headers: @headers, params: JSON.generate(body))
+
+          expect(response.status).to eq 404
+          message = JSON.parse(response.body, symbolize_names: true )
+          expect(message).to have_key(:error)
+          expect(message[:error]).to eq({details: "That api key does not exist"})
+        end
       end
     end
   end
-
 end
